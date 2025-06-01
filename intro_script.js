8d0e8f86e5faefa8e344f2f677fa454eab7ccbdb
joohun559 <joohun1007@gmail.com>
2025-06-01 09:50:17 +0900

    Add files via upload

--- intro_script.js
+++ intro_script.js
@@ -1,27 +1,29 @@
-// CRT 버튼에 마우스 누르면 스캔라인 켜기
-const crtButton = document.getElementById('crt-button');
-crtButton.addEventListener('mousedown', () => {
-  crtBox.classList.add('crt-active');
-});
-crtButton.addEventListener('mouseup', () => {
-  crtBox.classList.remove('crt-active');
-});
-crtButton.addEventListener('mouseleave', () => {
-  crtBox.classList.remove('crt-active');
-});
-
-const blinkLight = document.querySelector('.blink-light');
 const crtBox = document.querySelector('.crt-box');
+const blinkLight = document.querySelector('.blink-light');
+const crtButton = document.getElementById('crt-button');
+
 let powerState = 'on';
 let pressTimer = null;
+
+// CRT 버튼 눌렀을 때 스캔라인 효과 (PC + 모바일)
+function activateCRT() {
+  crtBox.classList.add('crt-active');
+}
+function deactivateCRT() {
+  crtBox.classList.remove('crt-active');
+}
+
+crtButton.addEventListener('mousedown', activateCRT);
+crtButton.addEventListener('mouseup', deactivateCRT);
+crtButton.addEventListener('mouseleave', deactivateCRT);
+crtButton.addEventListener('touchstart', activateCRT);
+crtButton.addEventListener('touchend', deactivateCRT);
 
 // 전원 끄기 효과
 function powerOff() {
   crtBox.classList.remove('power-on');
   crtBox.classList.add('power-off');
   powerState = 'off';
-
-  // 초록불을 빨간불로 변경
   blinkLight.classList.remove('green');
   blinkLight.classList.add('red');
 }
@@ -31,26 +33,30 @@ function powerOn() {
   crtBox.classList.remove('power-off');
   crtBox.classList.add('power-on');
   powerState = 'on';
-
-  // 빨간불을 초록불로 변경
   blinkLight.classList.remove('red');
   blinkLight.classList.add('green');
 }
 
-// 초록불 2초 누르면 꺼짐
-blinkLight.addEventListener('mousedown', () => {
+// 전원 끄기 타이머 시작 (2초 누름)
+function startPressTimer() {
   if (powerState === 'on') {
     pressTimer = setTimeout(powerOff, 2000);
   }
-});
-blinkLight.addEventListener('mouseup', () => {
-  clearTimeout(pressTimer);
-});
-blinkLight.addEventListener('mouseleave', () => {
-  clearTimeout(pressTimer);
-});
+}
 
-// 빨간불 클릭하면 켜짐
+// 타이머 취소
+function cancelPressTimer() {
+  clearTimeout(pressTimer);
+}
+
+// 초록불 2초간 눌렀을 때 꺼짐 (PC + 모바일)
+blinkLight.addEventListener('mousedown', startPressTimer);
+blinkLight.addEventListener('mouseup', cancelPressTimer);
+blinkLight.addEventListener('mouseleave', cancelPressTimer);
+blinkLight.addEventListener('touchstart', startPressTimer);
+blinkLight.addEventListener('touchend', cancelPressTimer);
+
+// 빨간불 클릭 또는 터치 시 전원 켜짐
 blinkLight.addEventListener('click', () => {
   if (powerState === 'off') {
     powerOn();
